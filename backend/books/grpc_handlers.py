7ce5a8824c3e794c18b0abd0180a8a2fa41a00b1
root <moijes12@gmail.com>
2026-04-30 11:30:37 +0000

    feat: Rebuilding for v1
    Deleting old files to rebuild for v1 which will have Django + REST

--- backend/books/grpc_handlers.py
+++
@@ -1,7 +0,0 @@
-# Add  grpc handler service registration 
-from django_socio_grpc.services.app_handler_registry import AppHandlerRegistry
-from books.grpc_services import BookService
-
-def grpc_handlers(server):
-    app_registry = AppHandlerRegistry("books",server)
-    app_registry.register(BookService)
