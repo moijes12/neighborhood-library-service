6a4a0f3953ad99d40a2c31a8648a66bb5ab3e809
root <moijes12@gmail.com>
2026-05-06 06:55:52 +0000

    feat: Moving routes to App
    Cleaning up main.tsx and moving routing to App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router';
-// import './index.css'
-// import App from './App.tsx'
-import BookList from './pages/BookList.tsx';
-import BookDetail from './components/books/BookDetail.tsx';
-import SignIn from './components/users/SignIn';
+import App from "./App"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<SignIn />} />
-        <Route path="books">
-          <Route index element={<BookList />} />
-          <Route path=":id" element={<BookDetail/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
